fb8c80092959ff468a9c356807e38b544aad80ba
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-07-09 17:23:45 -0300

    Exercício 14

--- Mundo 1/Python_Exercicios/ex014.py
+++ Mundo 1/Python_Exercicios/ex014.py
@@ -1,4 +1,4 @@
 grausC = float(input('Digite a temperatura em celsius: '))
 
-print('{} em celsius é igual a {} fahrenheit'.format(grausC, (grausC * 9 / 5) + 32))
-print('{} em celsius é igual a {} kelvin'.format(grausC, grausC + 273.15))
+print('{} em celsius corresponde a {} em fahrenheit'.format(grausC, (grausC * 9 / 5) + 32))
+print('{} em celsius corresponde a {} em kelvin'.format(grausC, grausC + 273.15))
