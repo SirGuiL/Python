89713b630d11bf31cafb4a6772468bd3880229f6
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-07-10 00:58:14 -0300

    Exercício 25

---
+++ Mundo 1/Python_Exercicios/ex025.py
@@ -0,0 +1,9 @@
+nome = input('Digite seu nome: ')
+
+if(nome.lower().find('silva') != -1):
+    print('Seu nome tem Silva.')
+
+else:
+    print('Seu nome não tem Silva.')
+
+nome = input('Digite seu nome: ')
