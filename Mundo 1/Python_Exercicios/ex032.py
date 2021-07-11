95e26831449935efe9d049dca81a3c13a4551206
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-07-11 00:04:46 -0300

    Exercício 32

--- Mundo 1/Python_Exercicios/ex032.py
+++ Mundo 1/Python_Exercicios/ex032.py
@@ -1,4 +1,9 @@
-ano = int(input('Digite um ano: '))
+from datetime import date
+
+ano = int(input('Digite um ano: (0 para utilizar o ano atual.) '))
+
+if ano == 0:
+    ano = date.today().year
 
 if ano % 4 == 0:
     print('{} é um ano bissexto'.format(ano))
