92b337ac418de4a1b547e68eea8fa125039d1014
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-07-13 00:25:21 -0300

    Aula 13

--- Mundo 2/aula_13/aula_13.py
+++ Mundo 2/aula_13/aula_13.py
@@ -29,3 +29,13 @@ for c in range(0, 6): # Laço for indo de 0 a 6
 
 for c in range(6, 0, -1): # Laço for indo de 6 a 0
     print(c)
+
+
+# palíndromo
+
+word = input('Digite uma palavra: ')
+
+if word.strip() == word.strip()[::-1]:
+    print('{} é um palíndromo'.format(word.strip().capitalize()))
+else: 
+    print('{} não é um palíndromo'.format(word.strip().capitalize()))
