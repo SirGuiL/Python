d685ab5656653b018862449bc083e62b40b32b76
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-07-16 23:06:20 -0300

    Exercício 66

---
+++ Mundo 2/Python_Exercicios/ex066.py
@@ -0,0 +1,10 @@
+num = soma = count = 0
+
+while True:
+    num = int(input('Digite um número: '))
+    if num == 999: break
+    count += 1
+    soma += num
+
+if count > 1: print(f'A soma entre os {count} números é {soma}')
+else: print(f'Você só digitou um número, logo a soma é {soma}')
