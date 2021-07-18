6101f42e77b81eb3a37d841d7ba119016ec6e63b
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-07-17 23:54:21 -0300

    Exercício 72

---
+++ Mundo 3/Python_Exercicios/ex072.py
@@ -0,0 +1,29 @@
+contagem = (
+    'zero',
+    'um',
+    'dois',
+    'três',
+    'quatro',
+    'cinco',
+    'seis',
+    'sete',
+    'oito',
+    'nove',
+    'dez',
+    'onze',
+    'doze',
+    'treze',
+    'quatorze',
+    'quinze',
+    'dezesseis',
+    'dezessete',
+    'dezoito',
+    'dezenove',
+    'vinte'
+)
+
+num = int(input('Digite um número entre 0 e 20: '))
+
+print(contagem[num])
+
+a = input('a')
