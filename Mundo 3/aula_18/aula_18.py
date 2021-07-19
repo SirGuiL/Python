cdea949441a7b5eda9d27596ee9ac058a424a2da
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-07-19 18:24:55 -0300

    Aula 18

---
+++ Mundo 3/aula_18/aula_18.py
@@ -0,0 +1,23 @@
+# Listas (parte 2)
+
+dados = list()
+pessoa = list()
+
+dados.append('Guilherme')
+dados.append(18)
+
+pessoa.append(dados[:])
+
+print(pessoa)
+print(pessoa[0][0]) # Retorna Guilherme
+print(pessoa[0][1]) # Retorna 18
+
+pessoa.clear() # Limpa a lista
+print(pessoa)
+
+galera = [['Guilherme', 18], ['Ana', 22], ['João', 56], ['Maria', 29]]
+
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos.')
+
+a = input('a')
