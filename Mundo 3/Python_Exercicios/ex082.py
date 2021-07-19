b6371f09a29c47c911037d44ba4ff29115e2759b
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-07-18 21:59:39 -0300

    Exercício 82

--- Mundo 3/Python_Exercicios/ex082.py
+++ Mundo 3/Python_Exercicios/ex082.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 lista = []
 pares = []
 ímpares = []
@@ -10,16 +12,24 @@ while True:
     num = int(input('Digite um número inteiro: '))
     lista.append(num)
 
-    if num % 2 == 0:
-        pares.append(num)
-    else:
-        ímpares.append(num)
-
     while True:
         opc = input('Quer continuar? [S/N] ').upper()
 
         if opc in 'SN':
             break
+
+for count in range(0, 2):
+    if count == 0:
+        print('Analisando os números...')
+    else:
+        print('Processando os números pares e ímpares...')
+    sleep(1)
+
+for c in lista:
+    if c % 2 == 0:
+        pares.append(c)
+    else:
+        ímpares.append(c)
 
 pares.sort()
 ímpares.sort()
