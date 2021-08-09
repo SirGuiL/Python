a7196ae666d8be35a49ed7cf3c966e19432885d8
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-08-09 12:07:42 -0300

    Exercício 96

---
+++ Mundo 3/Python_Exercicios/ex096.py
@@ -0,0 +1,11 @@
+def area(width, length):
+    print(f'A área do terreno é {width * length}m²')
+
+
+# programa principal
+width = float(input('Digite a largura do terreno: '))
+length = float(input('Digite o comprimento do terreno: '))
+
+area(width, length)
+
+a = input('a')
